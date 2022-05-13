f02c352d305ee8a5c016a5f2bc68fe7266d8c020
BerezentesvEvgeny <jonn09@icloud.com>
2022-05-13 09:41:56 +0300

    Initial Commit

---
+++ PhotoSafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoSafe
+//
+//  Created by Евгений Березенцев on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
